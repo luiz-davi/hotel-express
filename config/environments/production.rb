5084b39f5150b0287e1633a9365b106d0e37b335
luiz-davi <luizd4398@gmail.com>
2022-12-08 15:09:06 -0300

    setup railwayapp v1.9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "hotel-express-production-277a.up.railway.app"
+  config.hosts << "hotel-express-production.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
